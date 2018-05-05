9fc151c4a90b1dc4b3fea3ea7955034f8543e090
Olena Novitska <olenanovi@gmail.com>
2018-05-05 18:02:20 +0300

    Added Euro class. This is accounting money in euro.

---
+++ BlueberryPurse/BlueberryPurse/Currency/Euro.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueberryPurse.Currency
+{
+    public class Euro
+    {
+        private double moneyCount;
+        private double exchangeCourse;
+
+        public Euro()
+        {
+            moneyCount = 0.0;
+            exchangeCourse = 0.0;
+        }
+
+        public Euro(double count, double course)
+        {
+            moneyCount = count;
+            exchangeCourse = course;
+        }
+
+        public double ConvertEuro(Euro count)
+        {
+            return count.moneyCount * count.exchangeCourse;
+        }
+
+    }
+}
