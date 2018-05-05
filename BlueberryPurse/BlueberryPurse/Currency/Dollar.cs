6bbfe7a0adcc8b2a1d29ffe848fa5fe1c90e58bb
Alex <sashagsaban@gmail.com>
2018-05-05 18:04:56 +0300

    added class 'Dollar' (it can convert some amount to/from dollards based on an exchange rate)

---
+++ BlueberryPurse/BlueberryPurse/Currency/Dollar.cs
@@ -0,0 +1,49 @@
+﻿namespace BlueberryPurse.Currency
+{
+    public class Dollar
+    {
+        public double ExchangeRate
+        {
+            get;
+            private set;
+        }
+
+        public Dollar()
+            : this(1.0)
+        {
+        }
+
+        public Dollar(double exchangeRate)
+        {
+            ExchangeRate = exchangeRate;
+        }
+
+        public double ConvertDollars(double dollarsAmount)
+        {
+            var amount = dollarsAmount * ExchangeRate;
+
+            return amount;
+        }
+
+        public double ConvertDollars(double dollarsAmount, double exchangeRate)
+        {
+            var amount = dollarsAmount * exchangeRate;
+
+            return amount;
+        }
+
+        public double ConvertToDollars(double amount)
+        {
+            var dollarsAmount = amount / ExchangeRate;
+
+            return dollarsAmount;
+        }
+
+        public double ConvertToDollars(double amount, double exchangeRate)
+        {
+            var dollarsAmount = amount / exchangeRate;
+
+            return dollarsAmount;
+        }
+    }
+}
