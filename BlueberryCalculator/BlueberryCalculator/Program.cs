af91283a1fd02b31a13dcefa149597733c2ca708
Alex <sashagsaban@gmail.com>
2018-05-17 01:35:18 +0300

    Created one of the three module for Blueberries Calculator: graphical interface - Windows Forms solution

---
+++ BlueberryCalculator/BlueberryCalculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BlueberryCalculator
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //TODO: Run the form of calculator below.
+            //Application.Run(/* new CalculatorForm() */);
+        }
+    }
+}
