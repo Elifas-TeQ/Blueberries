2cdd309b282739385f8232344656b1e4fe99c6b6
Oleksandr Chaban <sashagsaban@gmail.com>
2018-05-16 19:44:05 +0300

    implemented Math.cs (consists all required mathematical functions) for CalcClass.dll (mathematical opertaions module).

---
+++ CalcClass/CalcClass/Math.cs
@@ -0,0 +1,68 @@
+﻿using SystemMath = System.Math;
+
+namespace CalcClass
+{
+    public static class Math
+    {
+        private static string _lastError = "";
+
+        public static string lastError => _lastError;
+        
+        #region Methods
+        public static int Add(long a, long b)
+        {
+            var result = a + b;
+
+            var integerResult = (int)result;
+
+            return integerResult;
+        }
+
+        public static int Sub(long a, long b)
+        {
+            return Add(a, -b);
+        }
+
+        public static int Mult(long a, long b)
+        {
+            var result = a * b;
+
+            var integerResult = (int)result;
+
+            return integerResult;
+        }
+
+        public static int Div(long a, long b)
+        {
+            var result = a / b;
+
+            var integerResult = (int)result;
+
+            return integerResult;
+        }
+
+        public static int Mod(long a, long b)
+        {
+            SystemMath.DivRem(a, b, out long result);
+
+            var integerResult = (int)result;
+
+            return integerResult;
+        }
+
+        public static int ABS(long a)
+        {
+            var result = SystemMath.Abs(a);
+
+            var integerResult = (int)result;
+
+            return integerResult;
+        }
+
+        public static int IABS(long a)
+        {
+            return -ABS(a);
+        }
+        #endregion
+    }
+}
