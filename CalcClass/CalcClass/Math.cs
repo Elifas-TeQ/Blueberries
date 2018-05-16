eceb44d378f11803babc907fab41200ba18aa3da
Oleksandr Chaban <sashagsaban@gmail.com>
2018-05-16 20:39:49 +0300

    implemented Math.cs with errors and without throwing exceptions (consists all required mathematical functions) for CalcClass.dll (mathematical opertaions module).

--- CalcClass/CalcClass/Math.cs
+++ CalcClass/CalcClass/Math.cs
@@ -1,4 +1,5 @@
-﻿using SystemMath = System.Math;
+﻿using System;
+using SystemMath = System.Math;
 
 namespace CalcClass
 {
@@ -8,60 +9,131 @@ namespace CalcClass
 
         public static string lastError => _lastError;
         
-        #region Methods
+        #region Public methods
         public static int Add(long a, long b)
         {
-            var result = a + b;
-
-            var integerResult = (int)result;
-
-            return integerResult;
+            return HandleBinaryOperation(a, b, () => a + b);
         }
 
         public static int Sub(long a, long b)
         {
-            return Add(a, -b);
+            return HandleBinaryOperation(a, b, () => a - b);
         }
 
         public static int Mult(long a, long b)
         {
-            var result = a * b;
-
-            var integerResult = (int)result;
-
-            return integerResult;
+            return HandleBinaryOperation(a, b, () => a * b);
         }
 
         public static int Div(long a, long b)
         {
-            var result = a / b;
+            var isError = CheckWhetherNumberIsZero(b);
 
-            var integerResult = (int)result;
+            if (isError)
+            {
+                return 0;
+            }
 
-            return integerResult;
+            return HandleBinaryOperation(a, b, () => a / b);
         }
 
         public static int Mod(long a, long b)
         {
-            SystemMath.DivRem(a, b, out long result);
+            var isError = CheckWhetherNumberIsZero(b);
 
-            var integerResult = (int)result;
+            if (isError)
+            {
+                return 0;
+            }
 
-            return integerResult;
+            return HandleBinaryOperation(a, b, () =>
+            {
+                SystemMath.DivRem(a, b, out long result);
+
+                return result;
+            });
         }
 
         public static int ABS(long a)
         {
-            var result = SystemMath.Abs(a);
+            return HandleUnaryOperation(a, () => SystemMath.Abs(a));
+        }
+
+        public static int IABS(long a)
+        {
+            return HandleUnaryOperation(a, () => -SystemMath.Abs(a));
+        }
+        #endregion
+
+        #region Private methods
+        private static int HandleUnaryOperation(long a, Func<long> operation)
+        {
+            var isError = !CheckWhetherNumberIsInIntegerRange(a, false);
+
+            if (isError)
+            {
+                return 0;
+            }
+
+            var result = operation();
 
             var integerResult = (int)result;
 
             return integerResult;
         }
 
-        public static int IABS(long a)
+        private static int HandleBinaryOperation(long a, long b, Func<long> operation)
         {
-            return -ABS(a);
+            var isError = !CheckWhetherNumberIsInIntegerRange(a) || !CheckWhetherNumberIsInIntegerRange(b);
+
+            if (isError)
+            {
+                return 0;
+            }
+
+            var result = operation();
+
+            isError = !CheckWhetherNumberIsInIntegerRange(result);
+
+            if (isError)
+            {
+                return 0;
+            }
+
+            var integerResult = (int)result;
+
+            return integerResult;
+        }
+
+        private static bool CheckWhetherNumberIsInIntegerRange(long number, bool couldBeMinValue = true)
+        {
+            var isNotInRange = number > int.MaxValue || number < int.MinValue;
+
+            if (!couldBeMinValue)
+            {
+                isNotInRange |= number <= int.MinValue;
+            }
+            
+            if (isNotInRange)
+            {
+                _lastError = "Error 06 - Дуже мале, або дуже велике значення числа для int.\nЧисла повинні бути в межах від -2147483648 до 2147483647.";
+
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool CheckWhetherNumberIsZero(long number)
+        {
+            if (number == 0.0)
+            {
+                _lastError = "Error 09 - Помилка ділення на 0.";
+
+                return true;
+            }
+
+            return false;
         }
         #endregion
     }
