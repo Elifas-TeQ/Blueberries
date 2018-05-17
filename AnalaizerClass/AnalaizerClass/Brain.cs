aacb0e364925c7db45961edbd719dc9cc48afd6a
Oleksandr Chaban <sashagsaban@gmail.com>
2018-05-17 19:44:37 +0300

    added Brain.cs to AnalaizerClass with all required methods by specification (without implementation).

---
+++ AnalaizerClass/AnalaizerClass/Brain.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+
+namespace AnalaizerClass
+{
+    public class Brain
+    {
+        private static int erposition = 0;
+
+        public static string expression = "";
+
+        public static bool ShowMessage = true;
+
+        public static bool CheckCurrency()
+        {
+            //TODO: implement.
+            return default(bool);
+        }
+
+        public static string Format()
+        {
+            //TODO: implement.
+            return string.Empty;
+        }
+
+        public static ArrayList CreateStack()
+        {
+            //TODO: implement.
+            return new ArrayList();
+        }
+
+        public static string RunEstimate()
+        {
+            //TODO: implement.
+            return string.Empty;
+        }
+
+        public static string Estimate()
+        {
+            //TODO: implement.
+            return string.Empty;
+        }
+    }
+}
