1b96085cbd9f1038d6962501ece7a900cfa5eb63
Olena Novitska <olenanovi@gmail.com>
2018-05-25 00:17:52 +0300

    Added forbiddance to change size of a window

--- BlueberryCalculator/BlueberryCalculator/CalcForm.Designer.cs
+++ BlueberryCalculator/BlueberryCalculator/CalcForm.Designer.cs
@@ -437,6 +437,7 @@
             this.Controls.Add(this.textBoxResult);
             this.Controls.Add(this.textBoxExpression);
             this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.KeyPreview = true;
             this.Margin = new System.Windows.Forms.Padding(5);
             this.MaximizeBox = false;
